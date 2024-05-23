ba09d82fa4155d7a2ec13c9ba4c10491b1b3d203
yamiladonzino <yamiladonzino51@gmail.com>
2024-05-23 12:24:34 -0300

    mejore la crud y elimine el patch

--- src/Usuario/usuario.controller.ts
+++ src/Usuario/usuario.controller.ts
@@ -5,49 +5,43 @@ import { User } from "./usuario.entity.js";
 const repository = new UserRepository()
 
 
-function sanitizeUserInput(req: Request, res:Response , next:NextFunction){
+function sanitizeUserInput(req: Request, res: Response , next: NextFunction){
+  req.body.sanitizedInput = {
+    name: req.body.name,
+    lastname: req.body.lastname,
+    dni: req.body.dni,
+    address: req.body.address,
+    mail: req.body.mail,
+    telephone: req.body.telephone,
+  }
 
-  req.body.sanitizedInput= {
-  name: req.body.name,
-  lastname: req.body.lastname,
-  dni: req.body.dni,
-  address: req.body.address,
-  mail: req.body.mail,
-  telephone: req.body.telephone,
-  
-}
-
-  Object.keys(req.body.sanitizedInput).forEach((key)=>{
+  Object.keys(req.body.sanitizedInput).forEach((key) => {
     if(req.body.sanitizedInput[key] === undefined){ 
-      delete req.body.sanitizedInput[key]}
-    })
-
+      delete req.body.sanitizedInput[key]
+    }
+  })
   next()
 }
 
+
 function findAll(req: Request ,res: Response){
-
-  res.json({data: repository.findAll()});
-
+  res.json({ data: repository.findAll() });
 }
 
+
 function findOne(req: Request ,res: Response){
-
-
-  const dni = req.params.dni
-  const user = repository.findOne({dni})
-
+  const id = req.params.id
+  const user = repository.findOne({ id })
+  
   if(!user){
-
-    return res.status(404).send({message: 'User doesnt found.'})
-
+    return res.status(404).send({message: 'User not found.'})
   }
 
   res.json({data: user})
 }
 
-function add(req: Request ,res: Response){
 
+function add(req: Request, res: Response){
   const input = req.body.sanitizedInput
 
   const userInput = new User(
@@ -60,34 +54,32 @@ function add(req: Request ,res: Response){
   )
 
   const user = repository.add(userInput)
-
   return res.status(201).send({message: 'User created', data: user})
-
 }
+
 
 function update(req: Request ,res: Response){
-    
-  req.body.sanitizedInput.dni = req.params.dni
-
+  req.body.sanitizedInput.id = req.params.id
   const user = repository.update(req.body.sanitizedInput)
 
-  if(!user){return res.status(404).send({message: "User not found"})}
-
-  return res.status(200).send({message: 'User updated succesfully.'})
-}
-
-function eliminate(req: Request ,res: Response){
-
-  const dni = req.params.dni
-
-  const user = repository.delete({dni})
-
-  if(!user){res.status(404).send({message: "User not found"})}
-
-  else{
-
-  return res.status(200).send({message: "User deleted succesfully"})}
-
+  if(!user){
+    return res.status(404).send({message: "User not found"})
   }
 
-export {findAll, findOne, add, eliminate, update, sanitizeUserInput}
+  return res.status(200).send({message: 'User updated succesfully.', data: user})
+}
+
+
+function eliminate(req: Request ,res: Response){
+  const id = req.params.id
+  const user = repository.delete({id})
+
+  if(!user) {
+    res.status(404).send({message: "User not found"})
+  } else{
+    return res.status(200).send({message: "User deleted succesfully"})
+  }
+
+}
+
+export { sanitizeUserInput, findAll, findOne, add, update, eliminate}
