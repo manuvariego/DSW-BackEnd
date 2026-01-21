173d5ea6e458d9f3e076055aedd88ec9a7ed2d5a
Juan Cardone <juancardone8@gmail.com>
2026-01-21 11:32:24 -0300

    metodos para traer PKSPCES

--- src/ParkingSpace/parkingSpace.routes.ts
+++ src/ParkingSpace/parkingSpace.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { findAll, findOne, update, add, eliminate, sanitizeParkingSpaceInput } from "./parkingSpace.controller.js";
+import { findAll, findOne, update, add, eliminate, sanitizeParkingSpaceInput, findAllofAGarage } from "./parkingSpace.controller.js";
 
 export const ParkingSpaceRouter = Router()
 
 ParkingSpaceRouter.get('/', findAll)
 ParkingSpaceRouter.get('/:number/:cuitGarage', findOne)
+ParkingSpaceRouter.get('/:cuitGarage', findAllofAGarage)
 ParkingSpaceRouter.post('/', sanitizeParkingSpaceInput, add)
 ParkingSpaceRouter.put('/:number/:cuitGarage', sanitizeParkingSpaceInput, update)
 ParkingSpaceRouter.delete('/:number/:cuitGarage', eliminate)
